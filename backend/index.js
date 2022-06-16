a307587d41420a13f0601e8b1ea896fbef69c2a4
Oskar Howe <oskar.howe@cosmotech.com>
2022-06-16 11:52:18 +0200

    styled, fetch graph.list in select element

--- backend/index.js
+++ backend/index.js
@@ -3,40 +3,48 @@ const server = express();
 const port = process.env.PORT || 25566;
 import { createClient } from "redis";
 
-let redis;
-
 //route index.html
 server.get("/", async function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+
   if (!redis) {
     //the client is not yet initialized
-    redis = createClient({
-      url: "redis://127.0.0.1:6379",
-    });
-    redis.on("error", (err) => console.log("Redis Client Error", err));
-    await redis.connect();
-    res.header("Access-Control-Allow-Origin", "*");
-    res.send("Conected to Redis-Stack");
+    res
+      .status(505)
+      .send("No connection between Backend and Redis-Stack established");
   } else {
-    res.header("Access-Control-Allow-Origin", "*");
     res.send("Already connected to Redis-Stack");
   }
 });
 //route graphs: returns a list of strings which are the keys of the graph entities
 server.get("/graphs", async function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+
   if (!redis) {
-    res.send("Can't get graphs if not connected to Redis-Stack");
+    //server error
+    res
+      .status(505)
+      .send("No connection between Backend and Redis-Stack established");
   } else {
     try {
       let graphs = await redis.graph.list();
       res.json(graphs);
     } catch (e) {
       console.log(e);
-      res.send("An error occured");
+      res.status(506).send("An error occured when fetching the graph entities");
     }
   }
 });
 
+let redis;
+redis = createClient({
+  url: "redis://127.0.0.1:6379",
+});
+redis.on("error", (err) => console.log("Redis Client Error", err));
+await redis.connect();
+console.log("Connection to Redis-Stack established");
+
 //open and accept connections
 server.listen(port, () =>
-  console.log(`Server is running and listening on port ${port}!`)
+  console.log(`Server is running and listening on  http://localhost:${port}!`)
 );
--- frontend/script.js
+++ frontend/script.js
@@ -1,15 +1,34 @@
-const textfield = document.getElementById("respose");
-textfield.innerHTML = "hello World";
-fetch("http://localhost:25566/") //because fetch returns a promise
+const textualView = document.getElementById("log");
+const graphSelect = document.getElementById("graph-select");
+
+function logString(string) {
+  let newP = document.createElement("p");
+  var text = document.createTextNode(string);
+  newP.appendChild(text);
+  textualView.appendChild(newP);
+}
+function populateGraphSelect(json) {
+  console.log(json);
+  let graphs = JSON.parse(JSON.stringify(json));
+  //let graphs = ["1", "2", "3"];
+  let options = graphs
+    .map((graph) => `<option value ${graph.toLowerCase()}>${graph}</option>}`)
+    .join("\n");
+
+  graphSelect.innerHTML = options;
+}
+
+fetch("http://localhost:25566/graphs") //because fetch returns a promise
   .then((response) => {
     if (!response.ok) {
       throw new Error(`HTTP error: ${response.status}`);
     }
-    return response.text();
+    return response.json();
   })
-  .then((text) => {
-    textfield.innerHTML = text;
+  .then((json) => {
+    populateGraphSelect(json);
+    logString("List of graph entities in Redis fetched");
   })
   .catch((error) => {
-    alert("Error in response: " + error);
+    logString(error);
   });
