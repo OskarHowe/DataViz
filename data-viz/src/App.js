ec7124016a8906a02c40e7b7d69e8fd67013fdc1
Oskar Howe <oskar.howe@cosmotech.com>
2022-06-28 11:53:58 +0200

    header, api access, dynamic render

--- data-viz/src/App.js
+++ data-viz/src/App.js
@@ -1,23 +1,40 @@
+import React, { useState, useEffect } from "react";
 import logo from "./cosmotechDark.png";
 import "./App.css";
 
 function App() {
+  const [graphRedisStrings, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:25566/graphs") //because fetch returns a promise
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((json) => {
+        console.log(`List of graph entities in Redis fetched: ${json}`);
+        setPosts(json);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
+        <div className="App-header-description">
+          <h1>Dataviz</h1>
+          <h2>Digitial Twin Management Solution</h2>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main>
+        {graphRedisStrings.map((string) => {
+          return <p>{string}</p>;
+        })}
+      </main>
     </div>
   );
 }
