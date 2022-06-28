afec99a22519afed57f38a69276f06dcb3b2bf46
Oskar Howe <oskar.howe@cosmotech.com>
2022-06-28 17:17:38 +0200

    modal with blue button and select input

--- data-viz/src/App.js
+++ data-viz/src/App.js
@@ -1,42 +1,53 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import logo from "./cosmotechDark.png";
+import Modal from "./components/Modal";
 import "./App.css";
 
-function App() {
-  const [graphRedisStrings, setPosts] = useState([]);
-  useEffect(() => {
-    fetch("http://localhost:25566/graphs") //because fetch returns a promise
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((json) => {
-        console.log(`List of graph entities in Redis fetched: ${json}`);
-        setPosts(json);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="App-header-description">
-          <h1>Dataviz</h1>
-          <h2>Digitial Twin Management Solution</h2>
-        </div>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <main>
-        {graphRedisStrings.map((string) => {
-          return <p>{string}</p>;
-        })}
-      </main>
-    </div>
-  );
+class App extends React.Component {
+  // const [graphRedisStrings, setPosts] = useState([]);
+  // useEffect(() => {
+  //   fetch("http://localhost:25566/graphs") //because fetch returns a promise
+  //     .then((response) => {
+  //       if (!response.ok) {
+  //         throw new Error(`HTTP error: ${response.status}`);
+  //       }
+  //       return response.json();
+  //     })
+  //     .then((json) => {
+  //       console.log(`List of graph entities in Redis fetched: ${json}`);
+  //       setPosts(json);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // }, []);
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalToggle: false,
+    };
+  }
+  // modalHanler(e) {
+  //   e.preventDefault();
+  //   this.setState({
+  //     modalToggle: true,
+  //   });
+  // }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="App-header-description">
+            <h1>Dataviz</h1>
+            <h2>Digitial Twin Management Solution</h2>
+          </div>
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <Modal></Modal>
+        <main className="graph-node"></main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ data-viz/src/components/Modal.js
@@ -0,0 +1,58 @@
+import React from "react";
+import "./Modal.css";
+import Select from "react-select";
+
+class Modal extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="Modal">
+        {/* <div id="closeX">+</div> */}
+        <h3>Display your Digital Twin Entity</h3>
+        <hr />
+        <div className="settingsElement">
+          <div className="settingsElement-text">
+            <h4>Choose a Graph Entity from our Database</h4>
+          </div>
+          <select id="graph-select" name="graph-select">
+            <option>No Option Available</option>
+          </select>
+          <div className="settingsElement-text">
+            <h4>Select an attribute to group the nodes </h4>
+            <p>
+              All nodes which hold the same value for the specified attribute
+              will be displayed as a compound node. The attribute can contain
+              numerical, categorical, or ordinal values
+            </p>
+          </div>
+          <select id="attribute-select" name="attribute-select">
+            <option>No Option Available</option>
+          </select>
+          <hr className="hr-paragraph" />
+
+          <div className="settingsElement-text">
+            <h4>Activate Algorithms to pre-group the graph</h4>
+          </div>
+
+          <div></div>
+          <p>Group sub trees in compounds</p>
+
+          <div></div>
+          <p>Group cliques and communities in compounds</p>
+          <div></div>
+          <div></div>
+          <button
+            className="blue-button"
+            onClick={() => alert("button clicked!")}
+          >
+            Visualize
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Modal;
