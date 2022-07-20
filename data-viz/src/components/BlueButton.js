8c657cbc36ccc3d70bcc161dc10b129894bafda0
Oskar Howe <oskar.howe@cosmotech.com>
2022-07-20 11:19:15 +0200

    refacto Blue Button

--- data-viz/src/components/BlueButton.js
+++ data-viz/src/components/BlueButton.js
@@ -2,20 +2,14 @@ import React from "react";
 import "./BlueButton.css";
 
 class BlueButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: props.text,
-    };
-  }
   render() {
     return (
       <button
         id={this.props.id}
         className="BlueButton"
-        onClick={this.props.onClick}
+        onClick={() => this.props.onClick()}
       >
-        {this.state.text}
+        {this.props.text}
       </button>
     );
   }
